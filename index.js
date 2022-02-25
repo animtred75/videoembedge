dff02113ac186b3bf1506807f104207e90c22c16
animtred75 <99289853+animtred75@users.noreply.github.com>
2022-02-25 02:54:38 -0500

    update2 index js
    s update2 index js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var vid = document.getElementById('vid');
-var fg = new URL(window.location).search.replace('?', '').split('&').forEach(function (p) {
+var fg = new URL(window.location).search.substr(1).split('&').forEach(function (p) {
     var parts = p.split('=');
     if (parts[0] == 'video_url') {
-        vid.src = parts[1];
+        vid.src = decodeURIComponent(parts[1]);
     } else {
         fetch('videoorg.json').then(async function (r) {
             var fh = await r.text();
