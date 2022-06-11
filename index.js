8851cc9ebcec67c904b297b9ab8f73224c17b606
animtred75 <99289853+animtred75@users.noreply.github.com>
2022-06-10 22:56:36 -0500

    add autoplay loog nocontrol

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 var vid = document.getElementById('vid');
-var ggfd = false;
-var fg = location.search.substr(1).split('&').forEach(function (p) {
+var fgdd = false;
+var fg = new URL(window.location).search.replace('?', '').split('&').forEach(function (p) {
     var parts = p.split('=');
-    if (parts[0] == 'video_url') {
-        ggfd = true;
+    if (parts[0] == 'autoplay') {
+        vid.autoplay = true;
+    } else if (parts[0] == 'nocontrols') {
+        vid.autoplay = true;
+        vid.controls = false;
+    }  else if (parts[0] == 'loop') {
+        vid.loop = true;
+    } else if (parts[0] == 'video_url') {
+        fgdd = true;
         vid.src = decodeURIComponent(parts[1]);
-    } else {
-        if (!ggfd) {
-            fetch('videoorg.json').then(async function (r) {
-                var fh = await r.text();
-                vid.src = JSON.parse(fh).video_origin;
-            })
-        }
     }
 });
+if (!fgdd) {
+    fetch('videoorg.json').then(async function (r) {
+        var fh = await r.text();
+        vid.src = JSON.parse(fh).video_origin;
+    })
+}
